88d5c4b893812a3c5f30c2e68012604d13a4b99c
junha6316 <junha6316@gmail.com>
2022-06-11 17:09:51 +0900

    추가: create-react-app

---
+++ README.md
@@ -0,0 +1,5 @@
+* 상태관리 패키지 한번 다 써보자
+- [ ] contextAPI
+- [ ] redux
+- [ ] recoil
+- [ ] mobx
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return null
+}
+
+export default App;
